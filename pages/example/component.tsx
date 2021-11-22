d59bf2883aabf2f02fed006925c24d0247783295
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2021-11-22 23:40:51 +0900

    feat(other): :sparkles: add ctx

--- pages/example/component.tsx
+++ pages/example/component.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from "next";
 import { Example } from "~/presenter/component/Example";
 import type { StaticProps } from "./type";
-import { CountProvider } from "~/presenter/component/Context/Count";
+import { CountProvider } from "~/presenter/component/Example/Context/Count";
+import { Count2Provider } from "~/presenter/component/Example/Context/Count2";
 
 export const Page: NextPage<StaticProps> = (props) => (
-  <CountProvider>
-    {props.name}
-    <Example />
-  </CountProvider>
+  <Count2Provider>
+    <CountProvider>
+      {props.name}
+      <Example />
+    </CountProvider>
+  </Count2Provider>
 );
