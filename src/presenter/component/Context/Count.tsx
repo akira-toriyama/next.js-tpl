31b60a4a698ca4d842390f045e85c01360bcf5f0
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2021-11-22 23:19:55 +0900

    feat(presenter): :sparkles: update ctx

---
+++ src/presenter/component/Context/Count.tsx
@@ -0,0 +1,41 @@
+import React, { useState, useContext } from "react";
+
+type CntType = {
+  i: number;
+};
+const CountStateContext = React.createContext<CntType>({ i: 0 });
+const CountDispatchContext = React.createContext<
+  React.Dispatch<React.SetStateAction<CntType>>
+>(() => undefined);
+
+/**
+ * stateとdispatchを分割する事でdispatch分の再レンダリングをが軽くなる
+ */
+export const CountProvider: React.FC = (props) => {
+  const [state, dispatch] = useState<CntType>({ i: 0 });
+
+  return (
+    <CountStateContext.Provider value={state}>
+      <CountDispatchContext.Provider value={dispatch}>
+        {props.children}
+      </CountDispatchContext.Provider>
+    </CountStateContext.Provider>
+  );
+};
+
+export const Count: React.VFC = () => {
+  // console.log("Count");
+  const { i } = useContext(CountStateContext);
+  return <p>cnt: {i}</p>;
+};
+
+export const Counter: React.VFC = () => {
+  // console.log("Counter");
+  const dispatch = useContext(CountDispatchContext);
+  return (
+    <>
+      <button onClick={() => dispatch((p) => ({ i: p.i - 1 }))}>-</button>
+      <button onClick={() => dispatch((p) => ({ i: p.i + 1 }))}>+</button>
+    </>
+  );
+};
--- src/presenter/component/Example/Example.tsx
+++ src/presenter/component/Example/Example.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import * as S from "./styles";
 import { useExample } from "./useExample";
+import { Count, Counter } from "~/presenter/component/Context/Count";
 
 export const Example: React.VFC = () => {
   const state = useExample();
+
   return (
     <>
+      <Count />
+      <Counter />
       {state.isSome && <>some</>}
       <div data-test-id="Example">
         <S.Txt>ExampleComponent</S.Txt>
