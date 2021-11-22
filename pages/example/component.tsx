7575b00d22c0c4784c2f2d2d990e75de150ca8cc
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2021-11-22 23:20:18 +0900

    feat(other): :sparkles: update page

--- pages/example/component.tsx
+++ pages/example/component.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
 import { Example } from "~/presenter/component/Example";
 import type { StaticProps } from "./type";
+import { CountProvider } from "~/presenter/component/Context/Count";
 
 export const Page: NextPage<StaticProps> = (props) => (
-  <>
+  <CountProvider>
     {props.name}
     <Example />
-  </>
+  </CountProvider>
 );
