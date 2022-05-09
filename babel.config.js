b970b2ffe482771b5d38fd48ba1f70209032dc70
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-05-09 12:53:21 +0900

    tweak(other): :fire: rm babel

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('@babel/core').TransformOptions} */
-module.exports = {
-  presets: ["next/babel"],
-  env: {
-    production: {
-      plugins: ["babel-plugin-jsx-remove-data-test-id"],
-    },
-  },
-};
