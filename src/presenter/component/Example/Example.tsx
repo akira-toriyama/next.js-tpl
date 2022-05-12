b3f1d0e5f472f9f41075ca4d0597dc60234cbba6
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-05-12 22:44:55 +0900

    refactor(presenter): :fire: rm style lib

--- src/presenter/component/Example/Example.tsx
+++ src/presenter/component/Example/Example.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { useExample } from "./useExample";
 import type { Props } from "./Example.type";
-import classes from "./Example.module.css";
 
 export const ExampleComponent: React.FC<Props> = (props) => (
   <>
     {props.isSome && <>some</>}
-    <div data-testid="Example" className={classes.txt}>
-      Example
-    </div>
+    <div data-testid="Example">Example</div>
   </>
 );
 
