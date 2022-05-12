4a26ceb5dc65b98a2b309a164ee31d526a283612
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-05-12 23:45:33 +0900

    add styled-components

--- next.config.js
+++ next.config.js
@@ -1,2 +1,14 @@
+// /** @type {import('next').NextConfig} */
+// module.exports = {
+//   ...require("@bird-studio/js-config").nextConfig,
+// };
+
 /** @type {import('next').NextConfig} */
-module.exports = require("@bird-studio/js-config").nextConfig;
+module.exports = {
+  reactStrictMode: true,
+  pageExtensions: ["page.tsx"],
+  compiler: {
+    reactRemoveProperties: true,
+    styledComponents: true,
+  },
+};
---
+++ src/presenter/component/Example/Example.style.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ButtonsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Button = styled.button`
+  background: palevioletred;
+  color: white;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  width: 100px;
+`;
