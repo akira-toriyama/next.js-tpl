bddfdf88dea74ced7617c0c4dc242edda8110f2d
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-06-28 23:30:41 +0900

    refactor: :recycle: fix props Close #528
    close test

--- .ladle/components.tsx
+++ .ladle/components.tsx
@@ -1,6 +1,6 @@
 import type { GlobalProvider } from "@ladle/react";
 import { Wrapper } from "~/ui/component/provider/Wrapper";
 
-export const Provider: GlobalProvider = ({ children }) => (
-  <Wrapper>{children}</Wrapper>
+export const Provider: GlobalProvider = (props) => (
+  <Wrapper>{props.children}</Wrapper>
 );
