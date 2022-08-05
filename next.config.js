f7bece1cb2f74fcbe12172059cf148778fc417dd
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-08-05 22:39:38 +0900

    refactor: :recycle: fix next conf

--- next.config.js
+++ next.config.js
@@ -1,2 +1,10 @@
 /** @type {import('next').NextConfig} */
-module.exports = require("@bird-studio/js-config").nextConfig;
+module.exports = {
+  reactStrictMode: true,
+  pageExtensions: ["page.tsx"],
+  compiler: {
+    // https://github.com/vercel/next.js/issues/36651#issuecomment-1126971095
+    reactRemoveProperties: process.env.NODE_ENV === "production",
+    styledComponents: true,
+  },
+};
