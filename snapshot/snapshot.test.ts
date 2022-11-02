45c305bf8d7b7aa2fae39090db078718e15baead
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-11-02 14:41:21 +0900

    xxx: :camera_flash: Update snapshot

--- snapshot/snapshot.test.ts
+++ snapshot/snapshot.test.ts
@@ -5,7 +5,9 @@ const url = "http://localhost:61000";
 const stories = fetch(`${url}/meta.json`).json().stories;
 
 Object.keys(stories).forEach((storyKey) => {
-  test(`${storyKey} - compare snapshots`, async ({ page }) => {
+  test(`${storyKey} - compare snapshots`, async ({ page }, testinfo) => {
+    testinfo.snapshotSuffix = "";
+
     await page.goto(`${url}/?story=${storyKey}&mode=preview`);
     await page.waitForSelector("[data-storyloaded]");
     await expect(page).toHaveScreenshot(`${storyKey}.png`);
