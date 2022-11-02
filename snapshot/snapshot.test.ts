7aaacf22d261432c29a8d74c50471fa3400a71a6
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-11-02 14:48:44 +0900

    xxx: :fire: Remove snapshot

--- snapshot/snapshot.test.ts
+++ snapshot/snapshot.test.ts
@@ -5,9 +5,7 @@ const url = "http://localhost:61000";
 const stories = fetch(`${url}/meta.json`).json().stories;
 
 Object.keys(stories).forEach((storyKey) => {
-  test(`${storyKey} - compare snapshots`, async ({ page }, testinfo) => {
-    testinfo.snapshotSuffix = "";
-
+  test(`${storyKey} - compare snapshots`, async ({ page }) => {
     await page.goto(`${url}/?story=${storyKey}&mode=preview`);
     await page.waitForSelector("[data-storyloaded]");
     await expect(page).toHaveScreenshot(`${storyKey}.png`);
