d4f74e93afdd1e037b8c993c873de5130667fd9c
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-11-04 10:57:58 +0900

    xxx: :alembic: `DesignSystem` demo

--- src/ui/general/Card/Card.stories.tsx
+++ src/ui/general/Card/Card.stories.tsx
@@ -2,6 +2,7 @@ import * as card from "./Card";
 
 export const Card: React.FC = () => (
   <>
+    test
     <card.Card {...{ title: "タイトル", body: "本文" }} />
     <card.Card {...{ title: "タイトル" }} />
   </>
