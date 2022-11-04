d13aad137ed0a6ed97dd92322bcb0d1a437cd373
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-11-04 11:23:19 +0900

    xxx: :alembic: Update story

--- src/ui/general/Card/Card.stories.tsx
+++ src/ui/general/Card/Card.stories.tsx
@@ -2,7 +2,7 @@ import * as card from "./Card";
 
 export const Card: React.FC = () => (
   <>
-    test
+    test👨‍💻
     <card.Card {...{ title: "タイトル", body: "本文" }} />
     <card.Card {...{ title: "タイトル" }} />
   </>
