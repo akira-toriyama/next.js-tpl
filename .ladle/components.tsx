d10c88f46a88872d160d230bbba44c3a95cd88e0
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-11-05 22:18:12 +0900

    xxx: :camera_flash: Update snapshot

--- .ladle/components.tsx
+++ .ladle/components.tsx
@@ -1,4 +1,6 @@
-import type { GlobalProvider } from "@ladle/react";
+import "normalize.css/normalize.css";
 import "./style.css";
+
+import type { GlobalProvider } from "@ladle/react";
 
 export const Provider: GlobalProvider = (props) => <>{props.children}</>;
