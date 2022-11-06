450db37e59e5003a7b3ec0036ee8ac857846a2b3
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-11-06 19:37:32 +0900

    xxo: :lipstick: Update stories

--- src/ui/general/Card/Card.stories.tsx
+++ src/ui/general/Card/Card.stories.tsx
@@ -1,8 +1,17 @@
 import * as card from "./Card";
+import type { Story } from "@ladle/react";
 
-export const Card: React.FC = () => (
+export const All: React.FC = () => (
   <>
     <card.Card {...{ title: "タイトル", body: "本文" }} />
     <card.Card {...{ title: "タイトル" }} />
   </>
 );
+
+type BasicProps = React.ComponentProps<typeof card.Card>;
+export const Basic: Story<BasicProps> = (props) => <card.Card {...props} />;
+
+Basic.args = ((): BasicProps => ({
+  title: "title",
+  body: "body",
+}))();
