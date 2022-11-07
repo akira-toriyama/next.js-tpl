961472645bf2d6cac747c366d3b2cd55a0f66fd5
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2022-11-07 17:28:13 +0900

    xxo: :bug: Fix build

--- src/ui/feature/item/Item/hook/item.hook.ts
+++ src/ui/feature/item/Item/hook/item.hook.ts
@@ -8,5 +8,16 @@ export const useItem: UseItem = () => {
   const { id } = useRouter().query;
   const { fetcher, opt } = service.determineFetcher({ id });
 
-  return service.toProps(useQuery(["Item", { id }], fetcher, opt));
+  // return service.toProps(useQuery(["Item", { id }], fetcher, opt));
+
+  const r = service.toProps(useQuery(["Item", { id }], fetcher, opt));
+
+  console.log(r);
+
+  return {
+    item: {
+      __tag: "success",
+      data: { id: "1", title: "title" },
+    },
+  };
 };
