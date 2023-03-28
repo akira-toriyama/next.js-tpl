112df439fd6cd0d7be8a5216d0502f5c9d6338d2
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2023-03-28 15:01:18 +0900

    :white_check_mark:[x.x.x](_): Add test

---
+++ src/ui/domain/item/Edit/Edit.test.tsx
@@ -0,0 +1,9 @@
+import { describe, test, expect } from "vitest";
+import { render } from "@testing-library/react";
+import { Edit } from "./Edit";
+
+describe.concurrent("Edit", () => {
+  test.concurrent("test", () => {
+    expect(render(<Edit {...{ id: "" }} />)).toBeDefined();
+  });
+});
