0db677dcf2eee5ca3671af7bb502907b0d402371
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2023-04-24 21:54:32 +0900

    :sparkles:[x.o.x](_): Add progress bar in page top

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { Wrapper } from "~/ui/provider/Wrapper";
+import NextNProgress from "nextjs-progressbar";
 
 type Props = AppProps<{ dehydratedState: unknown }>;
 const Page: NextPage<Props> = (props) => (
@@ -10,6 +11,7 @@ const Page: NextPage<Props> = (props) => (
     <Head>
       <meta name="viewport" content="width=device-width" />
     </Head>
+    <NextNProgress />
     <Wrapper {...props.pageProps}>
       <props.Component {...props.pageProps} />
     </Wrapper>
