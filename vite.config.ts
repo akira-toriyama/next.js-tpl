7739ae6e9479e7fdaa4506ee20a7ca95d6b33471
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2023-06-13 15:35:59 +0900

    :wrench:[x.x.o](_): Fix vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
-import { configDefaults } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 
 import react from "@vitejs/plugin-react";
 import * as path from "path";
