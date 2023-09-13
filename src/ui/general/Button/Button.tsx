9a5d5b16bdc6e9f01bd7e0324aec42ed2dad740b
鳥山明 <92862731+akira-toriyama@users.noreply.github.com>
2023-09-13 09:19:53 +0900

    :heavy_minus_sign:[x.x.o](_): Remove lib (#2160)
    * :heavy_minus_sign:[x.x.o](_): Remove lib
    
    * :camera_flash:[x.x.x](_): Update snapshot

--- src/ui/general/Button/Button.tsx
+++ src/ui/general/Button/Button.tsx
@@ -1,8 +1,6 @@
-import * as S from "./styled";
-
 type Props = React.DetailedHTMLProps<
   React.ButtonHTMLAttributes<HTMLButtonElement>,
   HTMLButtonElement
 >;
 
-export const Button: React.FC<Props> = (props) => <S.Button {...props} />;
+export const Button: React.FC<Props> = (props) => <button {...props} />;
--- src/ui/general/List/styled.tsx
+++
@@ -1,5 +0,0 @@
-import { styled } from "@kuma-ui/core";
-
-export const Li = styled("li")`
-  padding: 2px;
-`;
