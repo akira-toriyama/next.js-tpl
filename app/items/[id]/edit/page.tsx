b5014ba9498c815792d82061667a3e00f99480fb
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2023-09-24 02:07:12 +0900

    :bug:[x.x.o](_): Fix

--- app/items/[id]/edit/page.tsx
+++ app/items/[id]/edit/page.tsx
@@ -6,6 +6,7 @@ import * as repository from "~/ui/domain/item/_/repository";
 // export const dynamic = "force-dynamic";
 export const revalidate = 0;
 // export const fetchCache = "force-no-store";
+export const runtime = "edge";
 
 // const Page: NextPage<{ params: { id: string } }> = async (props) =>
 //   repository.fetchBy({ id: props.params.id }).then((r) =>
--- app/items/page.tsx
+++ app/items/page.tsx
@@ -6,6 +6,7 @@ import * as repository from "~/ui/domain/items/repository";
 // export const dynamic = "force-dynamic";
 export const revalidate = 0;
 // export const fetchCache = "force-no-store";
+export const runtime = "edge";
 
 // const Page: NextPage = () =>
 //   repository.fetchAll().then((r) =>
