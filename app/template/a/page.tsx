66d58e47a0aa1160a19f56d0bfa8f7037429f1da
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2023-09-24 01:14:13 +0900

    :bug:[x.x.o](_): Fix

--- app/template/a/page.tsx
+++
@@ -1,6 +0,0 @@
-import type { NextPage } from "next";
-import NextLink from "next/link";
-
-const Page: NextPage = () => <NextLink href="/template/b">bへ</NextLink>;
-
-export default Page;
--- app/template/b/page.tsx
+++
@@ -1,6 +0,0 @@
-import type { NextPage } from "next";
-import NextLink from "next/link";
-
-const Page: NextPage = () => <NextLink href="/template/a">aへ</NextLink>;
-
-export default Page;
