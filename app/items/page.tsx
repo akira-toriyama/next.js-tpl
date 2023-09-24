5ab7b4eb4b4f372ceeb222aed93d1c82a524d817
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2023-09-24 21:46:03 +0900

    :rotating_light:[x.x.x](_): Undo commented code

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -3,20 +3,12 @@ import { match, P } from "ts-pattern";
 import * as presenter from "~/ui/domain/items/presenter";
 import * as repository from "~/ui/domain/items/repository";
 
-// const Page: NextPage = () =>
-//   repository.fetchAll().then((r) =>
-//     match(r)
-//       .with({ items: P.nullish }, () => <presenter.Empty />)
-//       .with({ items: P.not(P.nullish) }, (rr) => <presenter.Items {...rr} />)
-//       .exhaustive(),
-//   );
-
-const Page: NextPage = () => (
-  <>
-    <div>
-      <h1>About Page</h1>
-    </div>
-  </>
-);
+const Page: NextPage = () =>
+  repository.fetchAll().then((r) =>
+    match(r)
+      .with({ items: P.nullish }, () => <presenter.Empty />)
+      .with({ items: P.not(P.nullish) }, (rr) => <presenter.Items {...rr} />)
+      .exhaustive(),
+  );
 
 export default Page;
