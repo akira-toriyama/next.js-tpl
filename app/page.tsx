d104536ee728203a2dde11b707bac6221f588288
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2023-09-24 21:36:50 +0900

    :white_check_mark:[x.x.x](_): Add e2e test

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,21 @@ import type { NextPage } from "next";
 import NextLink from "next/link";
 
 const Page: NextPage = () => (
-  <ul>
-    <li>
-      <NextLink href="/items">items</NextLink>
-    </li>
-    <li>
-      <NextLink href="/parallel-routes">parallel-routes</NextLink>
-    </li>
-    <li>
-      <NextLink href="/template/a">template</NextLink>
-    </li>
-  </ul>
+  <>
+    <ul>
+      <li>
+        <NextLink data-pw="items" href="/items">
+          items
+        </NextLink>
+      </li>
+      {/* <li>
+        <NextLink href="/parallel-routes">parallel-routes</NextLink>
+      </li> */}
+      {/* <li>
+        <NextLink href="/template/a">template</NextLink>
+      </li> */}
+    </ul>
+  </>
 );
 
 export default Page;
