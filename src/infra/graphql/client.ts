a594ec81183059e1e490ad5d2a52249950c7a9bc
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2023-09-24 10:57:32 +0900

    :bug:[x.x.o](_): Fix bug

--- src/infra/graphql/client.ts
+++ src/infra/graphql/client.ts
@@ -1,7 +1,6 @@
 import { GraphQLClient } from "graphql-request";
 import { env } from "~/env";
 
-export const client = new GraphQLClient(env.resources.endpoint).setHeader(
-  "authorization",
-  env.resources.authorization,
-);
+export const client = new GraphQLClient(env.resources.endpoint, {
+  cache: "no-cache",
+}).setHeader("authorization", env.resources.authorization);
