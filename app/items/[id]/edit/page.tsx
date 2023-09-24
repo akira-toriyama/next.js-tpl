96d11dff5818735143f81582e168a95aee87a21f
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2023-09-24 19:51:06 +0900

    :bulb:[x.x.x](_): Remove comment

--- app/items/[id]/edit/page.tsx
+++ app/items/[id]/edit/page.tsx
@@ -1,42 +1,14 @@
 import type { NextPage } from "next";
-// import { match, P } from "ts-pattern";
+import { match, P } from "ts-pattern";
 import * as presenter from "~/ui/domain/item/edit/presenter";
 import * as repository from "~/ui/domain/item/_/repository";
-// import { headers } from "next/headers";
 
-// export const dynamic = "force-dynamic";
-// export const revalidate = 0;
-// export const fetchCache = "default-no-store";
-
-// const Page: NextPage<{ params: { id: string } }> = async (props) =>
-//   repository.fetchBy({ id: props.params.id }).then((r) =>
-//     match(r)
-//       .with({ item: P.nullish }, () => <presenter.Empty />)
-//       .with({ item: P.not(P.nullish) }, (rr) => <presenter.ItemEdit {...rr} />)
-//       .exhaustive()
-//   );
-
-// export default Page;
-
-type Props = {
-  params: { id: string };
-};
-
-const f = async (props: Props) => {
-  // "use server";
-  // headers();
-  const r = await repository.fetchBy({ id: props.params.id });
-  return r;
-};
-
-const Page: NextPage<Props> = async (props) => {
-  const r = await f(props);
-
-  if (r.item == null) {
-    return <presenter.Empty />;
-  }
-
-  return <presenter.ItemEdit {...{ item: r.item }} />;
-};
+const Page: NextPage<{ params: { id: string } }> = async (props) =>
+  repository.fetchBy({ id: props.params.id }).then((r) =>
+    match(r)
+      .with({ item: P.nullish }, () => <presenter.Empty />)
+      .with({ item: P.not(P.nullish) }, (rr) => <presenter.ItemEdit {...rr} />)
+      .exhaustive(),
+  );
 
 export default Page;
