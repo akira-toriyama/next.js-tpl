727a1afc1f719b814bf751fc6680fcf0d2ea2314
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2023-10-03 13:13:51 +0900

    :lipstick:[x.x.o](_): Adoption Chakra UI

--- src/ui/domain/item/detail/presenter/ItemDetail.tsx
+++ src/ui/domain/item/detail/presenter/ItemDetail.tsx
@@ -1,4 +1,16 @@
-import NextLink from "next/link";
+import { Link } from "~/ui/general/Link";
+import {
+  ListItem,
+  CardBody,
+  UnorderedList,
+  Card,
+  CardHeader,
+  Heading,
+  Stack,
+  StackDivider,
+  Box,
+  Text,
+} from "@chakra-ui/react";
 
 export const Empty: React.FC = () => <>detail: Empty</>;
 
@@ -12,16 +24,53 @@ export const ItemDetail: React.FC<{
   };
 }> = (props) => (
   <>
-    <ul>
-      <li>
-        <NextLink href={`/items`}>items</NextLink>
-      </li>
-      <li>
-        <NextLink href={`/items/${props.item.id}/edit`}>edit</NextLink>
-      </li>
-    </ul>
-    <p>{props.item.id}</p>
-    <p>{props.item.title}</p>
-    <p>{props.item.body}</p>
+    <UnorderedList>
+      <ListItem>
+        <Link
+          {...{
+            href: `/items`,
+          }}
+        >
+          一覧へ
+        </Link>
+      </ListItem>
+      <ListItem>
+        <Link
+          {...{
+            href: `/items/${props.item.id}/edit`,
+          }}
+        >
+          編集へ
+        </Link>
+      </ListItem>
+    </UnorderedList>
+
+    <Card w="80" m="5">
+      <CardHeader>
+        <Heading size="md">Data detail</Heading>
+      </CardHeader>
+
+      <CardBody>
+        <Stack divider={<StackDivider />} spacing="4">
+          <Box>
+            <Heading size="xs" textTransform="uppercase">
+              Title
+            </Heading>
+            <Text pt="2" fontSize="sm">
+              {props.item.title}
+            </Text>
+          </Box>
+
+          <Box>
+            <Heading size="xs" textTransform="uppercase">
+              Body
+            </Heading>
+            <Text pt="2" fontSize="sm">
+              {props.item.body}
+            </Text>
+          </Box>
+        </Stack>
+      </CardBody>
+    </Card>
   </>
 );
--- src/ui/domain/item/edit/presenter/ItemEdit.tsx
+++ src/ui/domain/item/edit/presenter/ItemEdit.tsx
@@ -1,8 +1,19 @@
 "use client";
 
-import NextLink from "next/link";
 import type * as type from "./ItemEdit.type";
 import { useItemEdit } from "./hook";
+import {
+  ListItem,
+  UnorderedList,
+  FormErrorMessage,
+  FormLabel,
+  FormControl,
+  Input,
+  Box,
+  Button,
+  Center,
+} from "@chakra-ui/react";
+import { Link } from "~/ui/general/Link";
 
 export const Empty: React.FC = () => <>edit: 対象データ無し</>;
 
@@ -10,27 +21,77 @@ export const Loading: React.FC = () => <>edit: Loading</>;
 
 export const ItemEditComponent: React.FC<type.Props> = (props) => (
   <>
-    <ul>
-      <li>
-        <NextLink href={`/items`}>items</NextLink>
-      </li>
-      <li>
-        <NextLink href={`/items/${props.item.id}/detail`}>detail</NextLink>
-      </li>
-    </ul>
-    <form onSubmit={props.form.handleSubmit(props.onSubmit)}>
-      <p>{props.serverErrorMessage}</p>
+    <UnorderedList>
+      <ListItem>
+        <Link
+          {...{
+            href: `/items`,
+          }}
+        >
+          一覧へ
+        </Link>
+      </ListItem>
+      <ListItem>
+        <Link
+          {...{
+            href: `/items/${props.item.id}/edit`,
+          }}
+        >
+          編集へ
+        </Link>
+      </ListItem>
+    </UnorderedList>
+    <Box w="80" m="5">
+      <form onSubmit={props.form.handleSubmit(props.onSubmit)}>
+        <p>{props.serverErrorMessage}</p>
 
-      <input {...props.form.register("title")} name="title" type="input" />
-      <p>{props.form.formState.errors.title?.message}</p>
+        <FormControl
+          {...{
+            isRequired: true,
+            isInvalid: !!props.form.formState.errors.title,
+          }}
+        >
+          <FormLabel htmlFor={"title"}>タイトル</FormLabel>
+          <Input
+            {...{
+              ...props.form.register("title"),
+              id: "title",
+              // placeholder: '',
+              // maxLength: 10,
+            }}
+          />
+          <FormErrorMessage>
+            {props.form.formState.errors.title?.message}
+          </FormErrorMessage>
+        </FormControl>
 
-      <input {...props.form.register("body")} name="body" type="input" />
-      <p>{props.form.formState.errors.body?.message}</p>
+        <FormControl
+          {...{
+            isRequired: true,
+            isInvalid: !!props.form.formState.errors.body,
+          }}
+        >
+          <FormLabel htmlFor={"body"}>body</FormLabel>
+          <Input
+            {...{
+              ...props.form.register("body"),
+              id: "body",
+              // placeholder: '',
+              // maxLength: 10,
+            }}
+          />
+          <FormErrorMessage>
+            {props.form.formState.errors.body?.message}
+          </FormErrorMessage>
+        </FormControl>
 
-      <button type="submit" disabled={props.form.formState.isSubmitting}>
-        submit
-      </button>
-    </form>
+        <Center m="5">
+          <Button colorScheme="blue" type="submit">
+            submit
+          </Button>
+        </Center>
+      </form>
+    </Box>
   </>
 );
 
