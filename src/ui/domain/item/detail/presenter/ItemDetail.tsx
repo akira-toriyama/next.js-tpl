1510f1d9b3be6b4d5a4669e2322d937bc6316e43
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2023-10-08 15:45:58 +0900

    :camera_flash:[x.x.x](Item): Fix snapshot

--- src/ui/domain/item/detail/presenter/ItemDetail.tsx
+++ src/ui/domain/item/detail/presenter/ItemDetail.tsx
@@ -54,22 +54,14 @@ export const ItemDetail: React.FC<{
       <CardBody>
         <Stack divider={<StackDivider />} spacing="4">
           <Box>
-            <Heading size="xs">
-              Title
-              {/* TODO snapshot で失敗する */}
-              {/* {formParam.title.label} */}
-            </Heading>
+            <Heading size="xs">{formParam.title.label}</Heading>
             <Text pt="2" fontSize="sm">
               {props.item.title}
             </Text>
           </Box>
 
           <Box>
-            <Heading size="xs">
-              Body
-              {/* TODO snapshot で失敗する */}
-              {/* {formParam.body.label} */}
-            </Heading>
+            <Heading size="xs">{formParam.body.label}</Heading>
             <Text pt="2" fontSize="sm">
               {props.item.body}
             </Text>
