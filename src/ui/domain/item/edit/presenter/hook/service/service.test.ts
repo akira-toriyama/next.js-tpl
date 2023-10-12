c74180d538cde40f5eb6a42620865bc8af8dd3e4
akira-toriyama <92862731+akira-toriyama@users.noreply.github.com>
2023-10-12 21:43:07 +0900

    :white_check_mark:[x.x.x](Item): Update test

--- src/ui/domain/item/edit/presenter/hook/service/service.test.ts
+++ src/ui/domain/item/edit/presenter/hook/service/service.test.ts
@@ -1,97 +1,128 @@
-import { formSchema } from "./service";
+import { title, body } from "./service";
 import { describe, test, expect } from "vitest";
 import { formParam } from "../../../../_/presenter";
+import * as message from "~/ui/domain/_/message";
 
-describe.concurrent("formSchema", () => {
+describe.concurrent("title", () => {
   const validTitle = "".padStart(formParam.title.maxLength, "o");
-  const validBody = "".padStart(formParam.body.maxLength, "o");
 
   type TestData = {
     param: unknown;
-    expected: ReturnType<(typeof formSchema)["safeParse"]>;
+    expected: ReturnType<(typeof title)["safeParse"]>;
   };
 
   const testData: TestData[] = [
     {
-      param: {
-        title: "t",
-        body: "b",
-      },
+      param: validTitle,
       expected: {
         success: true,
-        data: expect.anything(),
+        data: validTitle,
       },
     },
     {
-      param: {
-        title: validTitle,
-        body: validBody,
-      },
+      param: `  ${validTitle}  `,
       expected: {
         success: true,
-        data: expect.anything(),
+        data: validTitle,
       },
     },
     {
-      param: {},
+      param: `x${validTitle}`,
       expected: {
         success: false,
-        error: expect.anything(),
+        error: {
+          issues: [
+            // @ts-expect-error
+            {
+              message: message.errorMessages.maxLength({
+                length: formParam.title.maxLength,
+              }),
+            },
+          ],
+        },
       },
     },
     {
-      param: {
-        title: "",
-        body: "",
-      },
+      param: "",
       expected: {
         success: false,
-        error: expect.anything(),
-      },
-    },
-    {
-      param: {
-        title: "t",
-        body: "",
-      },
-      expected: {
-        success: false,
-        error: expect.anything(),
-      },
-    },
-    {
-      param: {
-        title: "",
-        body: "b",
-      },
-      expected: {
-        success: false,
-        error: expect.anything(),
-      },
-    },
-    {
-      param: {
-        title: `${validTitle}-`,
-        body: validBody,
-      },
-      expected: {
-        success: false,
-        error: expect.anything(),
-      },
-    },
-    {
-      param: {
-        title: validTitle,
-        body: `${validBody}-`,
-      },
-      expected: {
-        success: false,
-        error: expect.anything(),
+        error: {
+          issues: [
+            // @ts-expect-error
+            {
+              message: message.errorMessages.minLength({
+                length: formParam.title.minLength,
+              }),
+            },
+          ],
+        },
       },
     },
   ];
 
   test.each(testData)("I/O %o", ({ param, expected }) => {
-    expect(formSchema.safeParse(param)).toEqual(expected);
+    expect(title.safeParse(param)).toMatchObject(expected);
+  });
+});
+
+describe.concurrent("body", () => {
+  const validBody = "".padStart(formParam.body.maxLength, "o");
+
+  type TestData = {
+    param: unknown;
+    expected: ReturnType<(typeof body)["safeParse"]>;
+  };
+
+  const testData: TestData[] = [
+    {
+      param: validBody,
+      expected: {
+        success: true,
+        data: validBody,
+      },
+    },
+    {
+      param: `  ${validBody}  `,
+      expected: {
+        success: true,
+        data: validBody,
+      },
+    },
+    {
+      param: `x${validBody}`,
+      expected: {
+        success: false,
+        error: {
+          issues: [
+            // @ts-expect-error
+            {
+              message: message.errorMessages.maxLength({
+                length: formParam.body.maxLength,
+              }),
+            },
+          ],
+        },
+      },
+    },
+    {
+      param: "",
+      expected: {
+        success: false,
+        error: {
+          issues: [
+            // @ts-expect-error
+            {
+              message: message.errorMessages.minLength({
+                length: formParam.body.minLength,
+              }),
+            },
+          ],
+        },
+      },
+    },
+  ];
+
+  test.each(testData)("I/O %o", ({ param, expected }) => {
+    expect(body.safeParse(param)).toMatchObject(expected);
   });
 });
--- src/ui/domain/item/edit/presenter/hook/service/service.ts
+++ src/ui/domain/item/edit/presenter/hook/service/service.ts
@@ -3,31 +3,35 @@ import * as type from "../../ItemEdit.type";
 import { formParam } from "../../../../_/presenter";
 import * as message from "~/ui/domain/_/message";
 
+export const title = z
+  .string()
+  .trim()
+  .min(formParam.title.minLength, {
+    message: message.errorMessages.minLength({
+      length: formParam.title.minLength,
+    }),
+  })
+  .max(formParam.title.maxLength, {
+    message: message.errorMessages.maxLength({
+      length: formParam.title.maxLength,
+    }),
+  });
+
+export const body = z
+  .string()
+  .trim()
+  .min(formParam.body.minLength, {
+    message: message.errorMessages.minLength({
+      length: formParam.body.minLength,
+    }),
+  })
+  .max(formParam.body.maxLength, {
+    message: message.errorMessages.maxLength({
+      length: formParam.body.maxLength,
+    }),
+  });
+
 export const formSchema: z.ZodSchema<type.FormValue> = z.object({
-  title: z
-    .string()
-    .trim()
-    .min(formParam.title.minLength, {
-      message: message.errorMessages.minLength({
-        length: formParam.title.minLength,
-      }),
-    })
-    .max(formParam.title.maxLength, {
-      message: message.errorMessages.maxLength({
-        length: formParam.title.maxLength,
-      }),
-    }),
-  body: z
-    .string()
-    .trim()
-    .min(formParam.body.minLength, {
-      message: message.errorMessages.minLength({
-        length: formParam.body.minLength,
-      }),
-    })
-    .max(formParam.body.maxLength, {
-      message: message.errorMessages.maxLength({
-        length: formParam.body.maxLength,
-      }),
-    }),
+  title,
+  body,
 });
